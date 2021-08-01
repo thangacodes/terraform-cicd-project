c5e0edbbe201b35ff1e57eb56a59fafc3893d536
send2durai <send2durai@hotmail.com>
2021-08-01 23:24:59 +0530

    initial commit

---
+++ resources.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "web-cluster" {
+  ami = var.ami
+  instance_type = var.instancetype
+  vpc_security_group_ids = [var.sgp[1]]
+  subnet_id = var.subnetid
+  tags = {
+    Name = var.name
+    Purpose = var.purpose
+    Tower = var.tower
+    Mail = var.mail
+  }
+}
+
+resource "aws_s3_bucket" "mybucket" {
+  bucket = var.bucketname
+}
---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "region" {
+  type = string
+}
+variable "accesskey" {
+  type = string
+}
+variable "secretkey" {
+  type = string
+}
+variable "ami" {
+  type = string
+}
+variable "instancetype" {
+  type = string
+}
+variable "sgp" {
+  type = list(string)
+}
+variable "subnetid" {
+  type = string
+}
+variable "name" {
+  type = string
+}
+variable "purpose" {
+  type = string
+}
+variable "tower" {
+  type = string
+}
+variable "mail" {
+  type = string
+}
+variable "bucketname" {
+  type = string
+}
