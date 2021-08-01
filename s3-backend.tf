da38dbfa6a3a554626addfffc94c3fe9f97bc949
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-08-02 00:26:36 +0530

    Update s3-backend.tf

--- s3-backend.tf
+++ s3-backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "my-terraform-backend-demo"
     key    = "EC2/Dev/ec2.tfstate"
-    region = "ap-south-1"
+    region = var.region
     dynamodb_table = "tf-s3-locl-table"
    }
 }
