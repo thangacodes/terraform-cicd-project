aecc50e2e3bbaed56431dd5eec1768b21d925a33
send2durai <send2durai@hotmail.com>
2021-08-01 23:28:12 +0530

    instance_type updated

--- resources.tf
+++ resources.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web-cluster" {
   ami = var.ami
   instance_type = var.instancetype
-  vpc_security_group_ids = [var.sgp[1]]
+  vpc_security_group_ids = [var.sgp[0]]
   subnet_id = var.subnetid
   tags = {
     Name = var.name
