b7d47283a05deaf59454a6e20552e229bfef793c
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-10-15 15:57:38 +0530

    Delete s3-backend.tf
    delete

--- s3-backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "my-terraform-backend-demo"
-    key    = "EC2/Dev/ec2.tfstate"
-    region = var.region
-    dynamodb_table = "tf-s3-locl-table"
-   }
-}
