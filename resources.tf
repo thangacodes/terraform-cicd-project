fb9b0520f426940b6f501b0c0063a9e80720f1bb
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-10-15 15:27:03 +0530

    Update resources.tf
    second commit

--- resources.tf
+++ resources.tf
@@ -1,16 +1,27 @@
-resource "aws_instance" "web-cluster" {
-  ami                    = var.ami
-  instance_type          = var.instancetype
+resource "aws_instance" "app-node" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [var.sgp[0]]
-  subnet_id              = var.subnetid
+  subnet_id              = var.subnet_id
+  key_name               = var.key_name
   tags = {
     Name    = var.name
-    Purpose = var.purpose
+    Env     = var.env
+    Project = var.project
     Tower   = var.tower
-    Mail    = var.mail
+    Owner   = var.owner
+  }
+
+  provisioner "remote-exec" {
+    inline = ["echo 'Wait until SSH is ready'"]
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      private_key = file("/home/centos/ondot.pem")
+      host = aws_instance.app-node.public_ip
   }
 }
-
-resource "aws_s3_bucket" "mybucket" {
-  bucket = var.bucketname
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ${aws_instance.app-node.public_ip}, --private-key /home/centos/ondot.pem /home/centos/remote-local-provisioner/httpd-install.yaml"
+}
 }
