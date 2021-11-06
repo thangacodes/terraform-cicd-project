d1ba843cd0efed81108ca8344e313c629e26bc94
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-11-06 15:07:37 +0530

    Delete output.tf

--- output.tf
+++
@@ -1,9 +0,0 @@
-### Declaring output values
-
-output "app-node-public_ip" {
-  value = aws_instance.app-node.public_ip
-}
-
-output "app-node-private_ip" {
-  value = aws_instance.app-node.private_ip
-}
